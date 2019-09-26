487808aae34a5e7bdf8b0c7b4fda7c7bf6d1da8f
valentynsson <valentynsson@gmail.com>
2019-09-26 20:32:02 +0000

    Freyja test

--- population.py
+++ population.py
@@ -1,12 +1,28 @@
+
+
+def pull_information_from_File():
+    
+    header = []
+    global_information = []
+
+    for first_line in file_:
+    header.append(first_line.split())
+    print(header)
+    
+    
+
+
+
+
 file_ = open("population.txt", "r")
+pull_information_from_File(file_)
 
 
-header = []
-global_information = []
 
-for first_line in file_:
-    header = first_line.split()
-    break
+
+
+
+
 
 
 

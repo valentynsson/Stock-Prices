928bc50c11e85f1d8ce5007f3ef03ade0da7730e
valentynsson <valentynsson@gmail.com>
2019-09-26 20:18:10 +0000

    Yoyo

---
+++ population.py
@@ -0,0 +1,30 @@
+file_ = open("population.txt", "r")
+
+
+header = []
+global_information = []
+
+for first_line in file_:
+    header = first_line.split()
+    break
+
+
+
+print(header)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# enterName = input("Please enter the year: ")
+
+
